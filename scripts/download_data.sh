f1ceddd1f6c14d91c4103203f1e89083bf635379
Jaume Palau <jpalauserrano@gmail.com>
2026-06-23 18:49:03 +0200

    Añado .gitignore y modifico script .download con condicional de vericacion de data para evitar duplicados

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+# Verifica si ya se tiene el dataset descargado en la rauta
+if [ -f data/raw/histopathologic-cancer-detection/train_labels.csv ]; then
+    echo "Dataset ya descargado"
+    exit 0
+fi
+
 # Crea el directorio para los datos
 mkdir -p data/raw 
+
+# Login a kaggle para poder hacer la descarga
+kaggle auth login
 
 # Descarga el zip en el directorio de data/raw
 kaggle competitions download \
   -c histopathologic-cancer-detection \
   -p data/raw
 
-
 # Descomprime el archivo en el mismo directorio
 unzip data/raw/histopathologic-cancer-detection.zip \
   -d data/raw/histopathologic-cancer-detection
+
+# Elimina el zip para liberar 7GB
+rm data/raw/histopathologic-cancer-detection.zip
